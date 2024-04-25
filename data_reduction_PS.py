d0b15b36573751012711331446b10acefa8c57f4
Pushkar Sunil Sathe <pushkarsathe89@gmail.com>
2024-04-25 18:14:20 -0400

    Neural Network testing 1. data_reduction_PS: updated and removed some hardcoded values. 2. home-credit-data-cleaning-nn.ipynb: Added - run a neural network on the master-data csv. 3. classification.py: Significantly improved to work with master data csv. Currently works only with numerical data.

--- data_reduction_PS.py
+++ data_reduction_PS.py
@@ -60,7 +60,8 @@ from features_dictionary import *
 #############################################################
 # Path to training data
 #############################################################
-path_train = 'data/csv_files/train/'
+path_train_read = 'data/csv_files/train/'
+path_train_save = 'data/csv_files/cleaned/'
 
 
 #############################################################
@@ -69,10 +70,10 @@ path_train = 'data/csv_files/train/'
 #############################################################
 # Function to replace categorical variables by int
 #############################################################
-def remove_strings(files):
+def remove_strings(files, path):
     print('REMOVING STRINGS')
     for i, file in enumerate(files):  # reading every file
-        df = pd.read_csv(path_train + file)  # importing data into a dataframe
+        df = pd.read_csv(path + file)  # importing data into a dataframe
 
         for column in df.columns:  # step through each column in the dataframe
             if df[column].dtype == 'object' and column[-1] != 'D':  # coversion to categorical for non-date columns
@@ -81,7 +82,7 @@ def remove_strings(files):
                 df[column].fillna('1899-12-31', inplace=True)
                 df[column] = pd.to_datetime(df[column], format='%Y-%m-%d')
                 df[column] = (df[column] - pd.Timestamp('1900-01-01 00:00:00')).apply(lambda x: x.days).copy()
-        df.to_csv(path_train + file, index=False)  # write new dataframe to file
+        df.to_csv(path_train_save + file, index=False)  # write new dataframe to file
 
         print(f' {i + 1}/{len(files)} ' + file)  # print progress
     print('=================')
@@ -94,10 +95,10 @@ def remove_strings(files):
 # Function to convert numbers to categorical variables by 
 # quartile.
 #############################################################
-def float_to_cat(files):
+def float_to_cat(files, path):
     print('CONVERTING FLOAT COLUMNS TO CATEGORICAL')
     for i, file in enumerate(files):  # reading every file
-        df = pd.read_csv(path_train + file)  # importing data into a dataframe
+        df = pd.read_csv(path_train_save + file)  # importing data into a dataframe
 
         for column in df.columns:  # step through each column in the dataframe
             if df[column].dtype == 'float64':  # check if the column is 'float64' type to be changed
@@ -111,7 +112,7 @@ def float_to_cat(files):
                 E = (df[column].isnull()) * -1  # NaN encoded as -1
                 df[column] = (A + B + C + D + E).copy().astype(np.int64)
 
-        df.to_csv('csv_files/train/' + file, index=False)  # write new dataframe to file
+        df.to_csv(path + file, index=False)  # write new dataframe to file
         print(f' {i + 1}/{len(files)} ' + file)  # print progress
     print('=================')
 
@@ -124,13 +125,13 @@ def float_to_cat(files):
 # num_group2. The aggregation function simply takes whatever
 # category appears the most.
 #############################################################
-def aggregate(files):
+def aggregate(files, path):
     print('AGGREGATING COLUMNS')
     for i, file in enumerate(files):  # reading every file
-        df = pd.read_csv(path_train + file, nrows=0).columns.tolist()  # read only the column names
+        df = pd.read_csv(path + file, nrows=0).columns.tolist()  # read only the column names
 
         if 'num_group1' in df:  # read in full file if num_group1 appears
-            df = pl.read_csv(path_train + file)
+            df = pl.read_csv(path + file)
             data = df.group_by('case_id').agg(pl.col(df.columns[1]).map_elements(lambda x: x.value_counts(sort=True)[
                 0, 0]))  # create dataframe that will aggregate by most frequent category
 
@@ -140,7 +141,7 @@ def aggregate(files):
                         0, 0]))  # aggregate remaining columns by most frequent category
                     data = data.join(df2, on='case_id')  # join newly created dataframe to the main one
 
-            data.write_csv(path_train + file)  # write new dataframe to file
+            data.write_csv(path + file)  # write new dataframe to file
 
         print(f'{i + 1}/{len(files)}')  # print progress
     print('=================')
@@ -152,17 +153,17 @@ def aggregate(files):
 #############################################################
 # Function that drops num_group1 and num_group2 columns.
 #############################################################
-def drop_num(files):
+def drop_num(files, path):
     print('DROPPING num_group1 AND num_group2 COLUMNS')
     for i, file in enumerate(files):  # reading every file
-        df = pl.read_csv(path_train + file)
+        df = pl.read_csv(path + file)
 
         if 'num_group2' in df.columns:
             df = df.drop('num_group1')  # drop num_group1
         if 'num_group2' in df.columns:
             df = df.drop('num_group2')  # drop num_group2
 
-        df.write_csv(path_train + file)
+        df.write_csv(path + file)
 
         print(f'{i + 1}/{len(files)} ' + file)  # print progress
     print('=================')
@@ -177,20 +178,30 @@ def drop_num(files):
 #############################################################
 def attach(columns, dataframes, df):
     print('ATTACHING COLUMNS')
+    df_results = pl.DataFrame()
+
     for i, column in enumerate(columns):  # go over every column that is not 'case_id'
         if column != 'case_id':
-            DF1 = pl.DataFrame()  # create an empty dataframe with two columns, 'case_id' and the current column
-            DF1.with_columns(pl.lit(None).alias('case_id'))
-            DF1.with_columns(pl.lit(None).alias(column))
+            # DF1 = pl.DataFrame()  # create an empty dataframe with two columns, 'case_id' and the current column
+            # DF1.with_columns(pl.lit(None).alias('case_id'))
+            # DF1.with_columns(pl.lit(None).alias(column))
+            DF1 = pl.DataFrame({'case_id': [], column: []})
+
             for d in dataframes:  # check every dataframe that has the same column name and concatenate the column to DF1
                 if column in d.columns:  # then aggregate by most frequent category as above
                     DF1 = pl.concat([DF1, d[['case_id', column]]])
-        DF1.group_by('case_id').agg(pl.col(column).map_elements(lambda x: x.value_counts(sort=True)[0, 0]))
-        df = df.join(DF1, on='case_id',
-                     how='left')  # join new column containing the aggregate of the same category spread over all dataframes to the master dataframe
+        DF1_agg = DF1.groupby('case_id').agg(
+            pl.col(column).map_elements(lambda x: x.value_counts(sort=True)[0, 0]).alias(column))
+        if df_results.height == 0:  # If it's the first processed column, initialize df_results
+            df_results = DF1_agg
+        else:
+            df_results = df_results.join(DF1_agg, on='case_id', how='left')
+        # DF1.group_by('case_id').agg(pl.col(column).map_elements(lambda x: x.value_counts(sort=True)[0, 0]))
+        # df = df.join(DF1, on='case_id',
+        #              how='left')  # join new column containing the aggregate of the same category spread over all dataframes to the master dataframe
         print(f'{i + 1}/{len(columns)}')  # print progress
     print('=================')
-    return df  # returns master dataframe
+    return df_results  # returns master dataframe
 
 
 #############################################################
@@ -201,16 +212,16 @@ def attach(columns, dataframes, df):
 #############################################################
 def modify_data(files, columns):
     a = time.time()
-    remove_strings(files[1:])  # modifies strings in all but train_base.csv file
-    float_to_cat(files[1:])  # float to categorical in all but train_base.csv file
-    aggregate(files[1:])  # aggregates as mentioned above in all but train_base.csv file
-    drop_num(files[1:])  # drops as mentioned above in all but train_base.csv file
-    D = [pl.read_csv(path_train + file) for file in files]  # read all over files in a list of dataframes
+    remove_strings(files[1:], path_train_read)  # modifies strings in all but train_base.csv file
+    float_to_cat(files[1:], path_train_save)  # float to categorical in all but train_base.csv file
+    aggregate(files[1:], path_train_save)  # aggregates as mentioned above in all but train_base.csv file
+    drop_num(files[1:], path_train_save)  # drops as mentioned above in all but train_base.csv file
+    D = [pl.read_csv(path_train_save + file) for file in files]  # read all over files in a list of dataframes
 
     # for column in df.columns:  # casts float columns as int
     #     if df[column].dtype == 'float64':
     #         df[column] = df[column].astype('int64').copy()
-    df = pl.read_csv(path_train + files[0])  # read in train_base.csv file
+    df = pl.read_csv(path_train_save + files[0])  # read in train_base.csv file
     df = attach(columns, D, df)  # begin attaching columns from all other dataframes to the main one from train_base.csv
     df = df.to_pandas()  # convert the dataframe to pandas
     df['date_decision'] = pd.to_datetime(df['date_decision'], format='%Y-%m-%d')  # convert datetime column
@@ -233,5 +244,6 @@ if __name__ == "__main__":
     #############################################################
     # Write final table to 'master_data_file.csv'
     #############################################################
-    df.to_csv('master_data_file.csv', index=False)
+    savepath = "/data/csv_files/master_data_file1.csv"
+    df.to_csv(savepath, index=False)
     #############################################################
--- src/classification.py
+++ src/classification.py
@@ -1,3 +1,4 @@
+from lightgbm import train
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,12 +7,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
+import time
+
+
+# Function to get the number of features from the dataloader
+def get_num_features(dataloader):
+    # Iterate over dataloader to get the first batch
+    for data, _ in dataloader:
+        return data.shape[1]  # Return the second dimension (num_features)
 
 
 class SimpleNN(nn.Module):
-    def __init__(self):
+    def __init__(self, size=466):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(500, 64)
+        self.fc1 = nn.Linear(size, 64)
         self.fc2 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 1)
 
@@ -22,54 +31,165 @@ class SimpleNN(nn.Module):
         return x
 
 
-def train_SimpleNN(X_train, y_train, epochs=100):
-    device = check_device()
-    # Initialize the model, loss function, and optimizer
-    model = SimpleNN()
-    # move model to device
-    model.to(device)
-    criterion = nn.BCELoss()  # Binary Cross-Entropy Loss for binary classification
+# def train_SimpleNN(X_train, y_train, epochs=100):
+#     device = check_device()
+#     # Initialize the model, loss function, and optimizer
+#     model = SimpleNN()
+#     # move model to device
+#     model.to(device)
+#     criterion = nn.BCELoss()  # Binary Cross-Entropy Loss for binary classification
 
+#     optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+#     X_train = torch.tensor(X_train).float().to(device)
+#     y_train = torch.tensor(y_train).float().to(device)
+
+#     # Training loop
+#     for epoch in range(epochs):  # Loop over the dataset multiple times
+#         optimizer.zero_grad()  # Zero the parameter gradients
+#         outputs = model(X_train)  # Forward pass
+#         loss = criterion(outputs, y_train)  # Calculate loss
+#         loss.backward()  # Backward pass
+#         optimizer.step()  # Optimize
+#         print(f"epoch {epoch} of {epochs} epochs")
+
+
+def train_SimpleNN(train_loader, test_loader, epochs=100):
+    device = check_device()
+    train_features = get_num_features(train_loader)
+    validation_features = get_num_features(test_loader)
+    print(f"Train_features: {train_features}")
+    assert (
+        train_features == validation_features
+    ), f"train features = {train_features} must be equal to validation features = {validation_features}"
+    model = SimpleNN(size=train_features)  # TODO: do this automatically.
+    model.to(device)
+    criterion = nn.BCELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    # Convert datasets to tensors if they're not already, and ensure the labels are float for BCELoss
-    X_train = torch.tensor(X_train).float().to(device)
-    y_train = torch.tensor(y_train).float().to(device)
+    # To store training and validation loss for monitoring
+    training_loss = []
+    validation_loss = []
+    start_time = time.time()
+    for epoch in range(epochs):
+        model.train()  # Set model to training mode
+        total_train_loss = 0
+        for X_batch, y_batch in train_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            optimizer.zero_grad()
+            outputs = model(X_batch)
+            loss = criterion(outputs, y_batch)
+            loss.backward()
+            optimizer.step()
+            total_train_loss += loss.item()
 
-    # Training loop
-    for epoch in range(epochs):  # Loop over the dataset multiple times
-        optimizer.zero_grad()  # Zero the parameter gradients
-        outputs = model(X_train)  # Forward pass
-        loss = criterion(outputs, y_train)  # Calculate loss
-        loss.backward()  # Backward pass
-        optimizer.step()  # Optimize
-        print(f"epoch {epoch} of {epochs} epochs")
+        # Calculate average training loss per epoch
+        avg_train_loss = total_train_loss / len(train_loader)
+        training_loss.append(avg_train_loss)
+
+        # Evaluation on the test set
+        model.eval()  # Set model to evaluation mode
+        total_val_loss = 0
+        with torch.no_grad():
+            for X_batch, y_batch in test_loader:
+                X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+                outputs = model(X_batch)
+                loss = criterion(outputs, y_batch)
+                total_val_loss += loss.item()
+
+        # Calculate average validation loss per epoch
+        avg_val_loss = total_val_loss / len(test_loader)
+        validation_loss.append(avg_val_loss)
+        epoch_duration = time.time() - start_time
+        print(
+            f"Epoch {epoch+1}/{epochs}, Training Loss: {avg_train_loss:.4f}, Validation Loss: {avg_val_loss:.4f} finished in {epoch_duration:.2f} seconds"
+        )
+
+    return training_loss, validation_loss
 
 
-def data_split(df, output):
+# def data_split(df, output):
+#     scaler = StandardScaler()
+#     X_scaled = scaler.fit_transform(df)
+#     # X_scaled = df
+
+#     # Splitting the data into training and testing sets
+#     # Using a test size of 20% and a random state for reproducibility
+#     X_train, X_test, y_train, y_test = train_test_split(X_scaled, output, test_size=0.2, random_state=42)
+
+#     # Displaying the sizes of the splits to verify
+#     print(f"Training data shape: {X_train.shape}")
+#     print(f"Testing data shape: {X_test.shape}")
+#     # Convert data to tensors
+#     train_dataset = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train.values).float())
+#     test_dataset = TensorDataset(torch.tensor(X_test).float(), torch.tensor(y_test.values).float())
+#     # Creating data loaders
+#     batch_size = 64
+#     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+#     # X_train = torch.randn(100, 500)  # 100 samples, 500 features -> directly train the joined table
+#     # y_train = torch.randint(0, 2, (100, 1)).type(torch.FloatTensor)  # Binary labels (0 or 1)
+#     return X_train, y_train#, train_loader, test_loader
+#     # train_model(train_loader, criterion, optimizer)
+# def data_split(df, output):
+#     scaler = StandardScaler()
+#     X_scaled = scaler.fit_transform(df)
+
+#     # Splitting the data into training and testing sets
+#     X_train, X_test, y_train, y_test = train_test_split(X_scaled, output, test_size=0.2, random_state=42)
+
+#     # Ensure that y_train and y_test are numpy arrays, not DataFrames
+#     y_train_array = y_train.values.to_numpy()  # This flattens the array if y_train is a DataFrame column
+#     y_test_array = y_test.values.to_numpy()    # Similarly for y_test
+
+#     # Convert data to tensors
+#     train_dataset = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train_array).float())
+#     test_dataset = TensorDataset(torch.tensor(X_test).float(), torch.tensor(y_test_array).float())
+
+#     # Creating data loaders
+#     batch_size = 64
+#     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+#     return train_loader, test_loader
+
+
+def data_split(df_in, output_in):
+    # Identify numeric columns (assuming non-numeric are object types or datetime)
+    df = df_in.select_dtypes(include=["number"])
+    # pd.DataFrame.select_dtypes
+    # df = df.values
+    # Scale only numeric columns
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(df)
+    # df_scaled[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+    dfvals = df.values
+    output = output_in.values
+    df_scaled = scaler.fit_transform(dfvals)
 
     # Splitting the data into training and testing sets
-    # Using a test size of 20% and a random state for reproducibility
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, output, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        df_scaled, output, test_size=0.2, random_state=42
+    )
 
-    # Displaying the sizes of the splits to verify
-    print(f"Training data shape: {X_train.shape}")
-    print(f"Testing data shape: {X_test.shape}")
+    # Ensure that y_train and y_test are numpy arrays
+    y_train_array = y_train
+    y_test_array = y_test
+    # y_test_array = y_test.values.ravel()
     # Convert data to tensors
-    train_dataset = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train.values).float())
-    test_dataset = TensorDataset(torch.tensor(X_test).float(), torch.tensor(y_test.values).float())
+    train_dataset = TensorDataset(
+        torch.tensor(X_train).float(), torch.tensor(y_train_array).float()
+    )
+    test_dataset = TensorDataset(
+        torch.tensor(X_test).float(), torch.tensor(y_test_array).float()
+    )
+
     # Creating data loaders
     batch_size = 64
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    # Assuming X_train and y_train are your training data and labels, respectively
-    X_train = torch.randn(100, 500)  # 100 samples, 500 features -> directly train the joined table
-    y_train = torch.randint(0, 2, (100, 1)).type(torch.FloatTensor)  # Binary labels (0 or 1)
-    return X_train, y_train#, train_loader, test_loader
-    # train_model(train_loader, criterion, optimizer)
+    return train_loader, test_loader
 
 
 def check_device():
@@ -92,14 +212,14 @@ if __name__ == "__main__":
     labels = np.random.randint(2, size=(num_rows, 1))  # Generates 0 or 1
     print(labels.shape)
     # Create DataFrame
-    df = pd.DataFrame(data, columns=['a', 'b', 'c', 'd'])
-    df['e'] = labels
+    df = pd.DataFrame(data, columns=["a", "b", "c", "d"])
+    df["e"] = labels
     print(df)
 
-    X = df[['a', 'b', 'c', 'd']]
-    y = df['e']  # Assuming 'e' is binary for classification
+    X = df[["a", "b", "c", "d"]]
+    y = df["e"]  # Assuming 'e' is binary for classification
 
-    X_train, y_train= data_split(X, y)
+    X_train, y_train = data_split(X, y)
 
     print("training")
     train_SimpleNN(X_train, y_train)
