Network improvements, evaluation and results: Updated data_reduction_PS and vectorization Fixed/updated Neural network, added logging, model saving, etc, Implemented calculation of metrics and plotting.

data_reduction_PS.py
@@ -37,7 +37,7 @@
 #    have introduced any bugs.
 #
 #############################################################
-
+import os.path
 
 #############################################################
 # Importing necessary modules
@@ -70,19 +70,24 @@ path_train_save = 'data/csv_files/cleaned/'
 #############################################################
 # Function to replace categorical variables by int
 #############################################################
-def remove_strings(files, path):
+def remove_strings(files, path, check_modified=True):
     print('REMOVING STRINGS')
     for i, file in enumerate(files):  # reading every file
-        df = pd.read_csv(path + file)  # importing data into a dataframe
+        if check_modified:
+            if os.path.exists(path_train_save + file):
+                continue
+        df_string = pd.read_csv(path + file)  # importing data into a dataframe
 
-        for column in df.columns:  # step through each column in the dataframe
-            if df[column].dtype == 'object' and column[-1] != 'D':  # coversion to categorical for non-date columns
-                df[column] = pd.factorize(df[column])[0].astype(np.int64)
-            elif df[column].dtype == 'object' and column[-1] == 'D':  # coversion to categorical for date columns
-                df[column].fillna('1899-12-31', inplace=True)
-                df[column] = pd.to_datetime(df[column], format='%Y-%m-%d')
-                df[column] = (df[column] - pd.Timestamp('1900-01-01 00:00:00')).apply(lambda x: x.days).copy()
-        df.to_csv(path_train_save + file, index=False)  # write new dataframe to file
+        for column in df_string.columns:  # step through each column in the dataframe
+            if df_string[column].dtype == 'object' and column[
+                -1] != 'D':  # coversion to categorical for non-date columns
+                df_string[column] = pd.factorize(df_string[column])[0].astype(np.int64)
+            elif df_string[column].dtype == 'object' and column[-1] == 'D':  # coversion to categorical for date columns
+                df_string[column].fillna('1899-12-31', inplace=True)
+                df_string[column] = pd.to_datetime(df_string[column], format='%Y-%m-%d')
+                df_string[column] = (df_string[column] - pd.Timestamp('1900-01-01 00:00:00')).apply(
+                    lambda x: x.days).copy()
+        df_string.to_csv(path_train_save + file, index=False)  # write new dataframe to file
 
         print(f' {i + 1}/{len(files)} ' + file)  # print progress
     print('=================')
@@ -95,24 +100,27 @@ def remove_strings(files, path):
 # Function to convert numbers to categorical variables by 
 # quartile.
 #############################################################
-def float_to_cat(files, path):
+def float_to_cat(files, path, check_modified=True):
     print('CONVERTING FLOAT COLUMNS TO CATEGORICAL')
     for i, file in enumerate(files):  # reading every file
-        df = pd.read_csv(path_train_save + file)  # importing data into a dataframe
+        if check_modified:
+            if os.path.exists(path_train_save + file):
+                continue
+        df_f2c = pd.read_csv(path_train_save + file)  # importing data into a dataframe
 
-        for column in df.columns:  # step through each column in the dataframe
-            if df[column].dtype == 'float64':  # check if the column is 'float64' type to be changed
-                _25 = df[column].describe()[4]  # 1st quartile boundary
-                _50 = df[column].describe()[5]  # 2nd
-                _75 = df[column].describe()[6]  # 3rd
-                A = (df[column] >= _75) * 3  # assigning variables by quartile
-                B = ((df[column] >= _50) & (df[column] < _75)) * 2
-                C = ((df[column] >= _25) & (df[column] < _50)) * 1
-                D = (df[column] < _25) * 0
-                E = (df[column].isnull()) * -1  # NaN encoded as -1
-                df[column] = (A + B + C + D + E).copy().astype(np.int64)
+        for column in df_f2c.columns:  # step through each column in the dataframe
+            if df_f2c[column].dtype == 'float64':  # check if the column is 'float64' type to be changed
+                _25 = df_f2c[column].describe()[4]  # 1st quartile boundary
+                _50 = df_f2c[column].describe()[5]  # 2nd
+                _75 = df_f2c[column].describe()[6]  # 3rd
+                A = (df_f2c[column] >= _75) * 3  # assigning variables by quartile
+                B = ((df_f2c[column] >= _50) & (df_f2c[column] < _75)) * 2
+                C = ((df_f2c[column] >= _25) & (df_f2c[column] < _50)) * 1
+                D = (df_f2c[column] < _25) * 0
+                E = (df_f2c[column].isnull()) * -1  # NaN encoded as -1
+                df_f2c[column] = (A + B + C + D + E).copy().astype(np.int64)
 
-        df.to_csv(path + file, index=False)  # write new dataframe to file
+        df_f2c.to_csv(path + file, index=False)  # write new dataframe to file
         print(f' {i + 1}/{len(files)} ' + file)  # print progress
     print('=================')
 
@@ -125,20 +133,25 @@ def float_to_cat(files, path):
 # num_group2. The aggregation function simply takes whatever
 # category appears the most.
 #############################################################
-def aggregate(files, path):
+def aggregate(files, path, check_modified):
     print('AGGREGATING COLUMNS')
     for i, file in enumerate(files):  # reading every file
-        df = pd.read_csv(path + file, nrows=0).columns.tolist()  # read only the column names
+        if check_modified:
+            if os.path.exists(path_train_save + file):
+                continue
+        df_aggregate = pd.read_csv(path + file, nrows=0).columns.tolist()  # read only the column names
 
-        if 'num_group1' in df:  # read in full file if num_group1 appears
-            df = pl.read_csv(path + file)
-            data = df.group_by('case_id').agg(pl.col(df.columns[1]).map_elements(lambda x: x.value_counts(sort=True)[
-                0, 0]))  # create dataframe that will aggregate by most frequent category
+        if 'num_group1' in df_aggregate:  # read in full file if num_group1 appears
+            df_aggregate = pl.read_csv(path + file)
+            data = df_aggregate.group_by('case_id').agg(
+                pl.col(df_aggregate.columns[1]).map_elements(lambda x: x.value_counts(sort=True)[
+                    0, 0]))  # create dataframe that will aggregate by most frequent category
 
-            for column in df.columns[2:]:
+            for column in df_aggregate.columns[2:]:
                 if column not in ['num_group1', 'num_group2']:
-                    df2 = df.group_by('case_id').agg(pl.col(column).map_elements(lambda x: x.value_counts(sort=True)[
-                        0, 0]))  # aggregate remaining columns by most frequent category
+                    df2 = df_aggregate.group_by('case_id').agg(
+                        pl.col(column).map_elements(lambda x: x.value_counts(sort=True)[
+                            0, 0]))  # aggregate remaining columns by most frequent category
                     data = data.join(df2, on='case_id')  # join newly created dataframe to the main one
 
             data.write_csv(path + file)  # write new dataframe to file
@@ -153,17 +166,20 @@ def aggregate(files, path):
 #############################################################
 # Function that drops num_group1 and num_group2 columns.
 #############################################################
-def drop_num(files, path):
+def drop_num(files, path, check_modified=True):
     print('DROPPING num_group1 AND num_group2 COLUMNS')
     for i, file in enumerate(files):  # reading every file
-        df = pl.read_csv(path + file)
+        if check_modified:
+            if os.path.exists(path_train_save + file):
+                continue
+        df_drop = pl.read_csv(path + file)
 
-        if 'num_group2' in df.columns:
-            df = df.drop('num_group1')  # drop num_group1
-        if 'num_group2' in df.columns:
-            df = df.drop('num_group2')  # drop num_group2
+        if 'num_group2' in df_drop.columns:
+            df_drop = df_drop.drop('num_group1')  # drop num_group1
+        if 'num_group2' in df_drop.columns:
+            df_drop = df_drop.drop('num_group2')  # drop num_group2
 
-        df.write_csv(path + file)
+        df_drop.write_csv(path + file)
 
         print(f'{i + 1}/{len(files)} ' + file)  # print progress
     print('=================')
@@ -203,47 +219,101 @@ def attach(columns, dataframes, df):
     print('=================')
     return df_results  # returns master dataframe
 
+import polars as pl
 
-#############################################################
+
+def process_dataframe(df, column):
+    return df.groupby('case_id').agg(
+        pl.col(column).mode().alias(column)
+    )
+
+
+# def attach(columns, dataframe_paths, base_df_path):
+#     print('ATTACHING COLUMNS')
+#     df_base = pl.read_csv(base_df_path)  # Load base dataframe
+#
+#     # Initialize result dataframe with the base 'case_id'
+#     df_results = df_base.select('case_id').unique()
+#
+#     for column in columns:
+#         try:
+#             if column != 'case_id':
+#                 DF1 = pl.DataFrame({'case_id': [], column: []})  # Temporary dataframe to aggregate column data
+#
+#                 for path in dataframe_paths:
+#                     df_temp = pl.scan_csv(path)  # Lazily load data
+#
+#                     if column in df_temp.columns:
+#                         # Select only necessary columns and append to DF1
+#                         df_processed = df_temp.select(['case_id', column]).collect()
+#                         DF1 = pl.concat([DF1, df_processed])
+#
+#                 # Aggregate data by 'case_id', selecting the most frequent value per case
+#                 DF1_agg = DF1.groupby('case_id').agg([
+#                     (pl.col(column).mode().alias(column))
+#                 ])
+#
+#                 # Join aggregated data back to results
+#                 df_results = df_results.join(DF1_agg, on='case_id', how='left')
+#         except Exception as e:
+#             print(f"{type(e)}: Column {column} cannot be concatenated. Skipping...")
+#     return df_results
 
 
 #############################################################
 # Main function that modifies all by the train_base.csv file
 #############################################################
-def modify_data(files, columns):
-    a = time.time()
-    remove_strings(files[1:], path_train_read)  # modifies strings in all but train_base.csv file
-    float_to_cat(files[1:], path_train_save)  # float to categorical in all but train_base.csv file
-    aggregate(files[1:], path_train_save)  # aggregates as mentioned above in all but train_base.csv file
-    drop_num(files[1:], path_train_save)  # drops as mentioned above in all but train_base.csv file
-    D = [pl.read_csv(path_train_save + file) for file in files]  # read all over files in a list of dataframes
-
+def modify_data(files, check_modified=True):
+    df = pd.read_csv(path_train_read+files[0])
+    df.to_csv(path_train_save + files[0])
+    remove_strings(files[1:], path_train_read, check_modified)  # modifies strings in all but train_base.csv file
+    float_to_cat(files[1:], path_train_save, check_modified)  # float to categorical in all but train_base.csv file
+    aggregate(files[1:], path_train_save,
+              check_modified)  # aggregates as mentioned above in all but train_base.csv file
+    drop_num(files[1:], path_train_save, check_modified)  # drops as mentioned above in all but train_base.csv file
     # for column in df.columns:  # casts float columns as int
     #     if df[column].dtype == 'float64':
     #         df[column] = df[column].astype('int64').copy()
-    df = pl.read_csv(path_train_save + files[0])  # read in train_base.csv file
-    df = attach(columns, D, df)  # begin attaching columns from all other dataframes to the main one from train_base.csv
-    df = df.to_pandas()  # convert the dataframe to pandas
-    df['date_decision'] = pd.to_datetime(df['date_decision'], format='%Y-%m-%d')  # convert datetime column
-    df.fillna(-1, inplace=True)  # replace all NaN by -1 that were introduced in the merging process
+
+
+def combine_data(files, columns, modify=True, check_modified=True):
+    a = time.time()
+    if modify:
+        modify_data(files, check_modified)
+        path_save = path_train_save
+    else:
+        path_save = path_train_read
+    # List of paths to your CSV files
+    dataframe_paths = [path_save + file for file in files]
+
+    # Attach columns from all dataframes to the base dataframe
+    df_combine = attach(columns, dataframe_paths, path_save + files[0])
+    df_combine = df_combine.to_pandas()  # convert the dataframe to pandas
+    df_combine['date_decision'] = pd.to_datetime(df_combine['date_decision'],
+                                                 format='%Y-%m-%d')  # convert datetime column
+    df_combine.fillna(-1, inplace=True)  # replace all NaN by -1 that were introduced in the merging process
     b = time.time()
     print('DONE!')
     print(b - a)
-    return df  # return master dataframe
+    return df_combine  # return master dataframe
 
 
 #############################################################
 
 if __name__ == "__main__":
+    modify = True
+
+    #############################################################
+
     #############################################################
     # Apply all of the above modifications to the dataset
     #############################################################
-    df = modify_data(FILES, COLUMNS)
+    df = combine_data(FILES, COLUMNS, modify=modify)
     #############################################################
 
     #############################################################
     # Write final table to 'master_data_file.csv'
     #############################################################
-    savepath = "/data/csv_files/master_data_file1.csv"
+    savepath = f"/data/csv_files/master_data_file{'' if modify else '_agg'}.csv"
     df.to_csv(savepath, index=False)
     #############################################################

src/classification.py
@@ -8,11 +8,19 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 import time
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 
 
 # Function to get the number of features from the dataloader
 def get_num_features(dataloader):
-    # Iterate over dataloader to get the first batch
+    """Returns the number of features in a dataloader
+
+    Args:
+        dataloader (DataLoader): Load dataloader
+
+    Returns:
+        num_feaatures: number of features
+    """
     for data, _ in dataloader:
         return data.shape[1]  # Return the second dimension (num_features)
 
@@ -30,31 +38,36 @@ class SimpleNN(nn.Module):
         x = torch.sigmoid(self.fc3(x))  # Sigmoid activation for binary classification
         return x
 
-
-# def train_SimpleNN(X_train, y_train, epochs=100):
-#     device = check_device()
-#     # Initialize the model, loss function, and optimizer
-#     model = SimpleNN()
-#     # move model to device
-#     model.to(device)
-#     criterion = nn.BCELoss()  # Binary Cross-Entropy Loss for binary classification
-
-#     optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-#     X_train = torch.tensor(X_train).float().to(device)
-#     y_train = torch.tensor(y_train).float().to(device)
-
-#     # Training loop
-#     for epoch in range(epochs):  # Loop over the dataset multiple times
-#         optimizer.zero_grad()  # Zero the parameter gradients
-#         outputs = model(X_train)  # Forward pass
-#         loss = criterion(outputs, y_train)  # Calculate loss
-#         loss.backward()  # Backward pass
-#         optimizer.step()  # Optimize
-#         print(f"epoch {epoch} of {epochs} epochs")
+class GRUModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(GRUModel, self).__init__()
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        # Initialize hidden state
+        h0 = torch.zeros(self.gru.num_layers, x.size(0), self.gru.hidden_size).to(x.device)
+        
+        # Forward propagate GRU
+        out, _ = self.gru(x, h0)
+        
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+        return out
 
 
 def train_SimpleNN(train_loader, test_loader, epochs=100):
+    """Trains a simple neural network given training and validation dataloaders
+
+    Args:
+        train_loader (DataLoader): DataLoader for training data
+        test_loader (DataLoader): DataLoader for validation data
+        epochs (int, optional): Number of epochs. Defaults to 100.
+
+    Returns:
+        Train Losses: list of train losses
+        Validation Losses: list of validation losses
+    """
     device = check_device()
     train_features = get_num_features(train_loader)
     validation_features = get_num_features(test_loader)
@@ -71,6 +84,10 @@ def train_SimpleNN(train_loader, test_loader, epochs=100):
     training_loss = []
     validation_loss = []
     start_time = time.time()
+    # Initialize variables to track the best model
+    best_val_loss = float('inf')
+    best_epoch = 0
+
     for epoch in range(epochs):
         model.train()  # Set model to training mode
         total_train_loss = 0
@@ -101,65 +118,89 @@ def train_SimpleNN(train_loader, test_loader, epochs=100):
         avg_val_loss = total_val_loss / len(test_loader)
         validation_loss.append(avg_val_loss)
         epoch_duration = time.time() - start_time
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            best_epoch = epoch
+            torch.save(model.state_dict(), 'W:/Erdos/Project/home_credit/data/bestSimpleNN.pth')  # Save the model parameters
+            print(f"Model saved at epoch {epoch+1} with Validation Loss: {avg_val_loss:.4f}")
+        print(f"Best model was saved at epoch {best_epoch+1} with a validation loss of {best_val_loss:.4f}")
+
         print(
             f"Epoch {epoch+1}/{epochs}, Training Loss: {avg_train_loss:.4f}, Validation Loss: {avg_val_loss:.4f} finished in {epoch_duration:.2f} seconds"
         )
 
     return training_loss, validation_loss
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+import matplotlib.pyplot as plt
 
+def evaluate_model(model, loader, plt_title=""):
+    """Evaluate a trained neural network on a test set and compute ROC AUC score.
 
-# def data_split(df, output):
-#     scaler = StandardScaler()
-#     X_scaled = scaler.fit_transform(df)
-#     # X_scaled = df
+    Args:
+        model (torch.nn.Module): The trained neural network model.
+        test_loader (DataLoader): DataLoader for validation or test data.
 
-#     # Splitting the data into training and testing sets
-#     # Using a test size of 20% and a random state for reproducibility
-#     X_train, X_test, y_train, y_test = train_test_split(X_scaled, output, test_size=0.2, random_state=42)
+    Returns:
+        float: The average loss on the test set.
+        list: The list of predicted probabilities.
+        float: The ROC AUC score for the predictions.
+    """
+    device = check_device()
+    model.to(device)
+    model.eval()  # Set the model to evaluation mode
+    criterion = nn.BCELoss()
+    total_loss = 0
+    y_true = []
+    y_scores = []
 
-#     # Displaying the sizes of the splits to verify
-#     print(f"Training data shape: {X_train.shape}")
-#     print(f"Testing data shape: {X_test.shape}")
-#     # Convert data to tensors
-#     train_dataset = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train.values).float())
-#     test_dataset = TensorDataset(torch.tensor(X_test).float(), torch.tensor(y_test.values).float())
-#     # Creating data loaders
-#     batch_size = 64
-#     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    with torch.no_grad():
+        for X_batch, y_batch in loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            outputs = model(X_batch)
+            loss = criterion(outputs, y_batch)
+            total_loss += loss.item()
+            y_true.extend(y_batch.cpu().numpy())
+            y_scores.extend(outputs.cpu().numpy())  # Collect raw probabilities
 
-#     # X_train = torch.randn(100, 500)  # 100 samples, 500 features -> directly train the joined table
-#     # y_train = torch.randint(0, 2, (100, 1)).type(torch.FloatTensor)  # Binary labels (0 or 1)
-#     return X_train, y_train#, train_loader, test_loader
-#     # train_model(train_loader, criterion, optimizer)
-# def data_split(df, output):
-#     scaler = StandardScaler()
-#     X_scaled = scaler.fit_transform(df)
+    average_loss = total_loss / len(loader)
+    
+    # Calculate ROC AUC
+    fpr, tpr, thresholds = roc_curve(y_true, y_scores)
+    roc_auc = auc(fpr, tpr)
+    # ROC curve    
+    fig =plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'Receiver Operating Characteristic : {plt_title}')
+    plt.legend(loc="lower right")
+    plt.show()
+    fig.savefig(f"W:/Erdos/Project/home_credit/data{plt_title}.png")
+    
+    # Print out the AUC score
+    print(f"ROC AUC Score: {roc_auc:.4f}")
+    
+    return average_loss, y_scores, roc_auc
 
-#     # Splitting the data into training and testing sets
-#     X_train, X_test, y_train, y_test = train_test_split(X_scaled, output, test_size=0.2, random_state=42)
+# average_loss, test_probabilities, roc_auc_score = evaluate_model(trained_model, test_loader)
 
-#     # Ensure that y_train and y_test are numpy arrays, not DataFrames
-#     y_train_array = y_train.values.to_numpy()  # This flattens the array if y_train is a DataFrame column
-#     y_test_array = y_test.values.to_numpy()    # Similarly for y_test
+def data_split(df_in: pd.DataFrame, output_in: pd.DataFrame, return_dfs:bool=False, seed:int=42):
+    """Split data into training and validation datasets or dataloaders
 
-#     # Convert data to tensors
-#     train_dataset = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train_array).float())
-#     test_dataset = TensorDataset(torch.tensor(X_test).float(), torch.tensor(y_test_array).float())
+    Args:
+        df_in (pd.DataFrame): Input Dataframe
+        output_in (pd.DataFrame): Output column from Dataframe
+        return_dfs (bool, optional): return split data arrays with columns instead of dataloaders. Defaults to True.
+        seed (int, optional): random state. Defaults to 42.
 
-#     # Creating data loaders
-#     batch_size = 64
-#     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-#     return train_loader, test_loader
-
-
-def data_split(df_in, output_in):
+    Returns:
+        _type_: Dataloaders or data arrays with column names.
+    """
     # Identify numeric columns (assuming non-numeric are object types or datetime)
     df = df_in.select_dtypes(include=["number"])
-    # pd.DataFrame.select_dtypes
-    # df = df.values
     # Scale only numeric columns
     scaler = StandardScaler()
     # df_scaled[numeric_cols] = scaler.fit_transform(df[numeric_cols])
@@ -169,27 +210,34 @@ def data_split(df_in, output_in):
 
     # Splitting the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(
-        df_scaled, output, test_size=0.2, random_state=42
+        df_scaled, output, test_size=0.2, random_state=seed
     )
+    if return_dfs:
+        X_train_df =pd.DataFrame(X_train, columns=df.columns)
+        X_test_df =pd.DataFrame(X_test, columns=df.columns)
+        y_train_df =pd.DataFrame(y_train, columns=output_in.columns)
+        y_test_df =pd.DataFrame(y_test, columns=output_in.columns)
+        print("Returning DFs")
+        return X_train_df, X_test_df, y_train_df, y_test_df
+    else:
+        # Ensure that y_train and y_test are numpy arrays
+        y_train_array = y_train
+        y_test_array = y_test
+        # y_test_array = y_test.values.ravel()
+        # Convert data to tensors
+        train_dataset = TensorDataset(
+            torch.tensor(X_train).float(), torch.tensor(y_train_array).float()
+        )
+        test_dataset = TensorDataset(
+            torch.tensor(X_test).float(), torch.tensor(y_test_array).float()
+        )
 
-    # Ensure that y_train and y_test are numpy arrays
-    y_train_array = y_train
-    y_test_array = y_test
-    # y_test_array = y_test.values.ravel()
-    # Convert data to tensors
-    train_dataset = TensorDataset(
-        torch.tensor(X_train).float(), torch.tensor(y_train_array).float()
-    )
-    test_dataset = TensorDataset(
-        torch.tensor(X_test).float(), torch.tensor(y_test_array).float()
-    )
+        # Creating data loaders
+        batch_size = 2000
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    # Creating data loaders
-    batch_size = 64
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-    return train_loader, test_loader
+        return train_loader, test_loader
 
 
 def check_device():